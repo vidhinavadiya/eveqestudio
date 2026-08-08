6eb7e705594c0cb36b104ec5312bc8b3559ef81d
Vidhinavadiya <vidhinavadiya90@gmail.com>
2026-08-08 22:30:54 +0530

    admin login

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json'
-  },
-  withCredentials: true
+  }
 });
 
-export const loginUser = (data) => API.post('/api/auth/login', data);
-
-export const registerUser = (data) => API.post('/api/auth/register', data);
+export const loginUser = (data) => API.post('/login', data);
+export const registerUser = (data) => API.post('/register', data);
